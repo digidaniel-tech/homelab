d5edfb6000d299213d0743391cdd3a56576b1c6f
Daniel Wollbro <wollbro90@gmail.com>
2024-02-28 14:51:29 +0100

    Uncomment these lines enables debug log

--- proxmox/terraform/_provider.tf
+++ proxmox/terraform/_provider.tf
@@ -13,4 +13,14 @@ provider "proxmox" {
   pm_api_url = var.PRX_URL
   pm_api_token_id = var.PRX_API_ID
   pm_api_token_secret = var.PRX_API_SECRET
+
+  # Enable this for debug logging
+  #
+  # pm_log_enable = true
+  # pm_log_file   = "terraform-plugin-proxmox.log"
+  # pm_debug      = true
+  # pm_log_levels = {
+  #   _default    = "debug"
+  #   _capturelog = ""
+  # }
 }
