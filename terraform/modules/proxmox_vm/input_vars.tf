3c697f3d83c343ea0e96e32599db69f3255f240c
Daniel Wollbro <wollbro90@gmail.com>
2024-03-04 23:36:08 +0100

    Added proxmox vm module

---
+++ terraform/modules/proxmox_vm/input_vars.tf
@@ -0,0 +1,63 @@
+variable "vm_count" {
+  description = "The amount of the VM instaces"
+  type = number
+  default = 1
+}
+
+variable "vm_name" {
+  description = "The name of the VM"
+  type = string
+}
+
+variable "vm_desc" {
+  description = "The description of the VM"
+  type = string
+}
+
+variable "vm_tags" {
+  description = "The tags for the VM"
+  type = string
+}
+
+variable "vm_iso" {
+  description = "The ISO that the VM should run"
+  type = string
+}
+
+variable "vm_host" {
+  description = "The host that the VM should run on"
+  type = string
+}
+
+variable "vm_cores" {
+  description = "The numer of CPU cores that the VM should run on"
+  type = number
+  default = 2
+}
+
+variable "vm_ram" {
+  description = "The amount of RAM that the VM should run on"
+  type = number
+  default = 2048
+}
+
+variable "vm_disk_size" {
+  description = "The size of the disk that the VM should run on"
+  type = number
+}
+
+variable "vm_disk_storage" {
+  description = "The location of the disk that the VM should run on"
+  type = string
+  default = "local-lvm"
+}
+
+variable "ssh_pub_key" {
+  description = "The public ssh key that will be used to access vm"
+  type = string
+}
+
+variable "ssh_default_password" {
+  description = "The default password that is used to ssh into VM"
+  type = string
+}
---
+++ terraform/modules/proxmox_vm/main.tf
@@ -0,0 +1,71 @@
+resource "proxmox_vm_qemu" "proxmox_vm_module" {
+  count = var.vm_count
+
+  name = var.vm_name
+  desc = var.vm_desc
+  agent = 1
+  tags = var.vm_tags
+  bios = "ovmf"
+  machine = "q35"
+
+  qemu_os = "l26"
+  iso = var.vm_iso
+  target_node = var.vm_host
+
+  full_clone = false
+
+  onboot = true
+  automatic_reboot = false
+
+  sockets = 1
+  cores = var.vm_cores
+  memory = var.vm_ram
+
+  scsihw = "virtio-scsi-single"
+
+  efidisk {
+    efitype = "4m"
+    storage = "local-lvm"
+  }
+
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          size = var.vm_disk_size
+          storage = var.vm_disk_storage
+        }
+      }
+    }
+  }
+  
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      disks,
+      vm_state,
+      network,
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p ~/.ssh",
+      "echo ${var.ssh_pub_key} >> ~/.ssh/authorized_keys",
+      "chmod 700 ~/.ssh",
+      "chmod 600 ~/.ssh/authorized_keys"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "daniel"
+      password    = "${ var.ssh_default_password }"
+      host        = self.default_ipv4_address
+      timeout     = "2m"
+    }
+  }
+}
