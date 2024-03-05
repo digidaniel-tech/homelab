072d02ecfb41e132345220452dc7038d0c12de17
Daniel Wollbro <wollbro90@gmail.com>
2024-03-05 11:51:16 +0100

    Proxmox terraform module support is missing

--- proxmox/terraform/105-dkr-prod.tf
+++ proxmox/terraform/105-dkr-prod.tf
@@ -1,16 +1,49 @@
-module "_105_dkr_prod_resource" {
-  source = "../../terraform/modules/proxmox_vm"
+resource "proxmox_vm_qemu" "_105_dkr_prod_resource" {
+  count = 1
+  
+  name = "Docker"
+  desc = "Docker instance to run containers from"
+  agent = 1
+  tags = "docker"
+  bios = "seabios"
+  machine = "pc"
 
-  vm_name = "Docker"
-  vm_desc = "Docker instance to run containers from"
-  vm_tags = "docker, proxy"
+  qemu_os = "l26"
+  iso = "local:iso/debian-12-unattended.iso"
+  target_node = var.PRX_HOST
 
-  vm_iso = "local:iso/debian-12-unattended.iso"
-  vm_host = var.PRX_HOST
+  full_clone = false
 
-  vm_disk_size = 50
-  vm_disk_storage = var.PRX_STORAGE_NAME
+  onboot = true
+  automatic_reboot = false
 
-  ssh_pub_key =  var.ssh_pub_key
-  ssh_default_password =  var.ssh_default_password
+  sockets = 1
+  cores = 2
+  memory = 2048
+
+  scsihw = "virtio-scsi-single"
+
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          size = 50
+          storage = var.PRX_STORAGE_NAME
+        }
+      }
+    }
+  }
+  
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      disks,
+      vm_state,
+      network,
+    ]
+  }
 }
